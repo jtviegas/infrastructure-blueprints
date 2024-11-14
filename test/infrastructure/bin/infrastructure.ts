8c7a5c3e6d817cca6bc7886e968dc0aa8f2357d8
joao tiago viegas <jtgv@novonordisk.com>
2024-11-14 14:10:44 +0100

    wip

--- test/infrastructure/bin/infrastructure.ts
+++ test/infrastructure/bin/infrastructure.ts
@@ -1,7 +1,6 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
-//import { Subdomains, SubdomainsProps } from '@jtviegas/cdk-blueprints';
 import {
   BaseConstructs, AppGwDistributedSpaProps,
   AppGwDistributedSpa,
